60ff55670bba8242dd5be1c5af8ca31743c40f3c
IhitplayzYT <ihitexe@gmail.com>
2026-06-07 17:11:40 +0530

    Finished parsing all async fns in a corpus and finished genertion of State Enums

--- src/helper.rs
+++ src/helper.rs
@@ -1,11 +1,14 @@
+#![allow(non_camel_case_types,non_snake_case,dead_code)]
+
 pub mod Helper {
     pub const OK: i32 = 0;
     pub const ERR: i32 = 1;
     pub const DBG_STR: &str = "";
 
-    struct CLI {
-        targets: Vec<String>,
-        dbg: bool,
+    #[derive(Debug,Clone,PartialEq)]
+    pub struct CLI {
+        pub targets: Vec<String>,
+        pub dbg: bool,
     }
 
     impl CLI {
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,15 @@
+use crate::{cstam::Cstam, helper::Helper};
+
 mod cstam;
 mod helper;
 
 fn main() {
-    println!("Hello, world!");
+    let args = Helper::Parse_Args();
+    if args.dbg {
+        println!("{:?}",args);
+    }
+    for i in args.targets{
+        Cstam::process_file(i);
+    }
+
 }
